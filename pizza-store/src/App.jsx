5e8929d696df739d05e81f631e60c61e0dfa6bd8
Cindy <210311822+clu1197@users.noreply.github.com>
2025-07-25 22:20:37 -0700

    feat: add React Router for client-side routing.

--- pizza-store/src/App.jsx
+++ pizza-store/src/App.jsx
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <h1>Hello vite</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
